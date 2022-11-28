173602fa45b4a9d15c08089400fbecfbfc7c4cd7
taoneswa kasirai <61433155+taoneswa@users.noreply.github.com>
2022-11-28 06:26:57 +0200

    Update App.py

--- App.py
+++ App.py
@@ -111,7 +111,7 @@ z-index:1;
 }
 </style>
 <div class="footer">
-<p>Developed by R204450W KASIRAI & R204438U CHAREKWA</p>
+<p>Developed by R204450W KASIRAI</p>
 </div>
 """
 st.markdown(footer,unsafe_allow_html=True)
